a039e7453029b5b26cea0231b5aa5147b09c9e4a
Lorenzo D'Ianni <lorenzo.dianni@qwentes.it>
2018-10-19 14:41:51 +0200

    refactor(build): setup build process for npm

--- build/index.js
+++ build/index.js
@@ -1,17 +1,17 @@
-const { basename } = require('path');
-const { argv } = require('yargs');
-const { execute } = require('./ExecuteCommand');
-const { createMainPackageJSON } = require('./CreateMainPackage');
-const { getDirectories } = require('./Util');
+const {basename} = require('path');
+const {argv} = require('yargs');
+const {build} = require('./build');
+const {createMainPackageJson} = require('./package');
+const {getChildDirectories} = require('./shared');
 
 const version = argv.v;
 const skipTest = argv.skipTest;
 const mainPackageJsonPath = `${__dirname}/../dist/package.json`;
-const directories = getDirectories(basename(`${__dirname}/../projects/`));
+const directories = getChildDirectories(basename(`${__dirname}/../projects/`));
 
 console.log('\x1b[32m%s\x1b[0m', `\nProjects: ${directories.join(', ')}`);
 
-execute([...directories], {version, skip: skipTest})
-  .then(() => createMainPackageJSON(version, mainPackageJsonPath))
+build(directories, {version, skipTest: skipTest})
+  .then(() => createMainPackageJson(version, mainPackageJsonPath))
   .then(() => console.log('\nFinish, bye :)'))
   .catch(console.log);
