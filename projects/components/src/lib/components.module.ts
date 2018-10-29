b8fdbdb4669c480c7bb162151c9b2629505866b5
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-10-29 11:05:10 +0100

    fix(chip): import module in components module

--- projects/components/src/lib/components.module.ts
+++ projects/components/src/lib/components.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {ToggleModule} from './toggle/toggle.module';
 import {ButtonModule} from './button/button.module';
-import {ChipComponent} from './chip/chip.component';
+import {ChipModule} from './chip/chip.module';
 import {DialogModule} from './dialog/dialog.module';
 
 @NgModule({
-  imports: [ToggleModule, ButtonModule, ChipComponent, DialogModule],
-  exports: [ToggleModule, ButtonModule, ChipComponent, DialogModule],
+  imports: [ToggleModule, ButtonModule, ChipModule, DialogModule],
+  exports: [ToggleModule, ButtonModule, ChipModule, DialogModule],
 })
-export class ComponentsModule {}
+export class ComponentsModule {
+}
