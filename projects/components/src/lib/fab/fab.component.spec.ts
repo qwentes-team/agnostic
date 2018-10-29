6ad33fcaf74c1a536799ef44c89299a8a9c61f5b
Matteo Barone <barmat90@gmail.com>
2018-10-29 15:42:23 +0100

    test(fab): transclude

--- projects/components/src/lib/fab/fab.component.spec.ts
+++ projects/components/src/lib/fab/fab.component.spec.ts
@@ -1,24 +1,77 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {FabComponent} from './fab.component';
+import {Component, DebugElement} from '@angular/core';
+import {getChildDebugElement} from '../../test.shared';
 
 describe('FabComponent', () => {
-  let component: FabComponent;
-  let fixture: ComponentFixture<FabComponent>;
+  let hostFixture: ComponentFixture<any>;
+  let hostComponent: any;
+  let hostElement: HTMLElement;
+  let fabDebugger: DebugElement;
 
-  beforeEach(async(() => {
+  const setupBeforeEachTestWithHostComponent = HostComponentClass => {
     TestBed.configureTestingModule({
-      declarations: [FabComponent],
-    }).compileComponents();
-  }));
+      declarations: [FabComponent, HostComponentClass],
+    });
+    hostFixture = TestBed.createComponent(HostComponentClass);
+    hostComponent = hostFixture.componentInstance;
+    hostElement = hostFixture.nativeElement;
+  };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FabComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  const getFabDebugger = () => getChildDebugElement('ag-fab').from(hostFixture);
+
+  afterEach(() => {
+    if (hostFixture && hostFixture.destroy) {
+      hostFixture.destroy();
+    }
+    hostFixture = null;
+    hostComponent = null;
+    hostElement = null;
+    fabDebugger = null;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('transclude', () => {
+    describe('[position=before]', () => {
+      @Component({
+        template: '<ag-fab><span position="before">Test before</span></ag-fab>',
+      })
+      class TestHostComponent {}
+
+      beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+      fit('should transclude before the .ag-fab__icon element', () => {
+        hostFixture.detectChanges();
+        fabDebugger = getFabDebugger();
+        const defaultTransclude = fabDebugger.nativeElement.querySelector(
+          '.ag-fab__icon'
+        );
+        expect(defaultTransclude.previousElementSibling.innerText).toBe(
+          'TEST BEFORE'
+        );
+      });
+    });
+
+    describe('[position=after]', () => {
+      @Component({
+        template: '<ag-fab><span position="after">Test after</span></ag-fab>',
+      })
+      class TestHostComponent {}
+
+      beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+      fit('should transclude after the .ag-fab__icon element', () => {
+        hostFixture.detectChanges();
+        fabDebugger = getFabDebugger();
+        const defaultTransclude = fabDebugger.nativeElement.querySelector(
+          '.ag-fab__icon'
+        );
+        expect(defaultTransclude.nextElementSibling.innerText).toBe(
+          'TEST AFTER'
+        );
+      });
+    });
   });
+
+  describe('position', () => {});
 });
