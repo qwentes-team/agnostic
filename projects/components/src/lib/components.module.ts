9cc56d3f6dc8ba2975ec4ef016300548549292d2
Matteo Barone <barmat90@gmail.com>
2018-10-30 11:13:11 +0100

    feat(input): initial commit | #10

--- projects/components/src/lib/components.module.ts
+++ projects/components/src/lib/components.module.ts
@@ -3,9 +3,10 @@ import {ToggleModule} from './toggle/toggle.module';
 import {ButtonModule} from './button/button.module';
 import {ChipModule} from './chip/chip.module';
 import {DialogModule} from './dialog/dialog.module';
+import {InputModule} from './input/input.module';
 
 @NgModule({
-  imports: [ToggleModule, ButtonModule, ChipModule, DialogModule],
-  exports: [ToggleModule, ButtonModule, ChipModule, DialogModule],
+  imports: [ToggleModule, ButtonModule, ChipModule, DialogModule, InputModule],
+  exports: [ToggleModule, ButtonModule, ChipModule, DialogModule, InputModule],
 })
 export class ComponentsModule {}
