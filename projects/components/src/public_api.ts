eb0bbef295e9ead415de950918db761c007173e2
Matteo Barone <barmat90@gmail.com>
2018-11-09 18:21:41 +0100

    chore(version): 1.1.1 | export DialogService

--- projects/components/src/public_api.ts
+++ projects/components/src/public_api.ts
@@ -7,4 +7,5 @@ export * from './lib/toggle/toggle.module';
 export * from './lib/button/button.module';
 export * from './lib/chip/chip.module';
 export * from './lib/dialog/dialog.module';
+export * from './lib/dialog/dialog.service';
 export * from './lib/fab/fab.module';
