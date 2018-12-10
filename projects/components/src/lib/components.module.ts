79b2de2aa8efbcc340d5d0de6daee9000a583536
Matteo Barone <barmat90@gmail.com>
2018-12-10 12:03:06 +0100

    feat(radio): initial commit | #17

--- projects/components/src/lib/components.module.ts
+++ projects/components/src/lib/components.module.ts
@@ -4,9 +4,10 @@ import {ButtonModule} from './button/button.module';
 import {ChipModule} from './chip/chip.module';
 import {DialogModule} from './dialog/dialog.module';
 import {FabModule} from './fab/fab.module';
+import {RadioModule} from './radio/radio.module';
 
 @NgModule({
-  imports: [ToggleModule, ButtonModule, ChipModule, DialogModule, FabModule],
-  exports: [ToggleModule, ButtonModule, ChipModule, DialogModule, FabModule],
+  imports: [ToggleModule, ButtonModule, ChipModule, DialogModule, FabModule, RadioModule],
+  exports: [ToggleModule, ButtonModule, ChipModule, DialogModule, FabModule, RadioModule],
 })
 export class ComponentsModule {}
---
+++ projects/components/src/lib/radio/radio.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'ag-radio',
+  templateUrl: './radio.component.html',
+  styleUrls: ['./radio.component.scss'],
+})
+export class RadioComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
