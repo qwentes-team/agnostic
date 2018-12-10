5972ef3a2063ef7fa00a8eb9dd7d04af004f4ed5
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-12-10 19:37:41 +0100

    feat(input): add story and fix some tests | close #10

--- projects/components/src/lib/input/input.component.spec.ts
+++ projects/components/src/lib/input/input.component.spec.ts
@@ -1,7 +1,7 @@
 import {Component, DebugElement, OnInit} from '@angular/core';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {InputComponent} from './input.component';
-import {getChildDebugElement, updateValueOfInput, updateValueOfSelect} from '../../test.shared';
+import {click, getChildDebugElement, updateValueOfInput, updateValueOfSelect} from '../../test.shared';
 import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 describe('InputComponent', () => {
@@ -30,6 +30,108 @@ describe('InputComponent', () => {
     hostComponent = null;
     hostElement = null;
     inputDebugger = null;
+  });
+
+  describe('Label', () => {
+    @Component({template: '<ag-input name="foo">Foo label!</ag-input>'})
+    class TestHostComponent {}
+
+    beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+    it('should transclude label', () => {
+      hostFixture.detectChanges();
+      inputDebugger = getInputDebugger();
+      const labelDOM = inputDebugger.nativeElement.querySelector('.ag-input__label');
+      expect(labelDOM.innerText).toBe('Foo label!');
+    });
+  });
+
+  describe('[metaLabel]', () => {
+    @Component({template: '<ag-input name="foo" metaLabel="super!">Foo label!</ag-input>'})
+    class TestHostComponent {}
+
+    beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+    it('should transclude label', () => {
+      hostFixture.detectChanges();
+      inputDebugger = getInputDebugger();
+      const labelDOM = inputDebugger.nativeElement.querySelector('.ag-input__label');
+      expect(labelDOM.querySelector('span').innerText).toBe('super!');
+    });
+  });
+
+  describe('(change)', () => {
+    @Component({template: '<ag-input (change)="onChange($event)">Foo label!</ag-input><button>noop</button>'})
+    class TestHostComponent {
+      event: Event;
+
+      onChange(e) {
+        this.event = e;
+      }
+    }
+
+    beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+    it('should emit changes', () => {
+      hostFixture.detectChanges();
+      inputDebugger = getInputDebugger();
+      spyOn(hostFixture.componentInstance, 'onChange').and.callThrough();
+      updateValueOfInput(inputDebugger.nativeElement.querySelector('input'), 'Foo', hostFixture).catch(console.log);
+      expect(hostFixture.componentInstance.onChange).toHaveBeenCalled();
+      expect(hostFixture.componentInstance.onChange.calls.count()).toBe(1);
+      expect(hostFixture.componentInstance.onChange.calls.argsFor(0)[0]).toEqual(hostFixture.componentInstance.event);
+    });
+  });
+
+  describe('(focus)', () => {
+    @Component({template: '<ag-input (focus)="onFocus($event)">Foo label!</ag-input><button>noop</button>'})
+    class TestHostComponent {
+      event: Event;
+
+      onFocus(e) {
+        this.event = e;
+      }
+    }
+
+    beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+    it('should emit focus', () => {
+      hostFixture.detectChanges();
+      inputDebugger = getInputDebugger();
+      spyOn(hostFixture.componentInstance, 'onFocus').and.callThrough();
+      const inputDOM = inputDebugger.nativeElement.querySelector('input');
+      inputDOM.dispatchEvent(new Event('focus'));
+      updateValueOfInput(inputDOM, 'Foo', hostFixture).catch(console.log);
+      expect(hostFixture.componentInstance.onFocus).toHaveBeenCalled();
+      expect(hostFixture.componentInstance.onFocus.calls.count()).toBe(1);
+      expect(hostFixture.componentInstance.onFocus.calls.argsFor(0)[0]).toEqual(hostFixture.componentInstance.event);
+    });
+  });
+
+  describe('(blur)', () => {
+    @Component({template: '<ag-input (blur)="onBlur($event)">Foo label!</ag-input><button>noop</button>'})
+    class TestHostComponent {
+      event: Event;
+
+      onBlur(e) {
+        this.event = e;
+      }
+    }
+
+    beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+    it('should emit blur', () => {
+      hostFixture.detectChanges();
+      inputDebugger = getInputDebugger();
+      spyOn(hostFixture.componentInstance, 'onBlur').and.callThrough();
+      const inputDOM = inputDebugger.nativeElement.querySelector('input');
+      updateValueOfInput(inputDOM, 'Foo', hostFixture).catch(console.log);
+      click(hostFixture.nativeElement.querySelector('button'));
+      inputDOM.dispatchEvent(new Event('blur'));
+      expect(hostFixture.componentInstance.onBlur).toHaveBeenCalled();
+      expect(hostFixture.componentInstance.onBlur.calls.count()).toBe(1);
+      expect(hostFixture.componentInstance.onBlur.calls.argsFor(0)[0]).toEqual(hostFixture.componentInstance.event);
+    });
   });
 
   describe('[name]', () => {
@@ -84,7 +186,7 @@ describe('InputComponent', () => {
     it('should prevent changes', () => {
       hostFixture.detectChanges();
       inputDebugger = getInputDebugger();
-      updateValueOfInput(inputDebugger.nativeElement, 'Baz', hostFixture).catch(console.log);
+      updateValueOfInput(inputDebugger.nativeElement.querySelector('input'), 'Baz', hostFixture).catch(console.log);
       expect(inputDebugger.nativeElement.querySelector('input').disabled).toBe(true);
       expect(inputDebugger.nativeElement.querySelector('input').value).toBe('');
     });
