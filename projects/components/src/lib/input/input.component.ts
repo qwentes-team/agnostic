97f65c53c5fa7260edee41cad75936883db9ce11
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-12-10 18:12:21 +0100

    feat(input): ag-input component

--- projects/components/src/lib/input/input.component.ts
+++ projects/components/src/lib/input/input.component.ts
@@ -1,9 +1,11 @@
 import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
+  ChangeDetectionStrategy, ChangeDetectorRef,
+  Component, forwardRef, Input,
   ViewEncapsulation,
 } from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {ToggleBoolean} from '../toggle/toggle.component';
+import {convertToBoolean, EmitToNgModel, noop} from '../components.shared';
 
 @Component({
   selector: 'ag-input',
@@ -11,9 +13,53 @@ import {
   styleUrls: ['./input.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent implements OnInit {
-  constructor() {}
+export class InputComponent implements ControlValueAccessor, EmitToNgModel {
+  @Input() name = '';
+  @Input() value = '';
+  @Input() disabled = false;
+  @Input() required = false;
+  @Input() metaLabel = '';
 
-  ngOnInit() {}
+  onChange = noop.onChange;
+  onTouched = noop.onTouched;
+
+  constructor(
+    private cd: ChangeDetectorRef
+  ) {
+  }
+
+  writeValue(value: string): void {
+    this.updateValue(value);
+  }
+
+  registerOnChange(fn: (value: boolean | any) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(disabled: ToggleBoolean): void {
+    this.disabled = convertToBoolean(disabled);
+    this.cd.detectChanges();
+  }
+
+  public emitToNgModel(newValue: string): void {
+    this.onChange(this.updateValue(newValue));
+  }
+
+  public updateValue(newValue: string): string {
+    this.value = newValue;
+    this.cd.detectChanges();
+    return this.value;
+  }
 }
