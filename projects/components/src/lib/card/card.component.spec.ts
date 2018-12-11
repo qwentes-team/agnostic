1c5d728d5040331d7d06ff25b7a90cdaaaaae725
Roberta Gardiman <roberta.gardiman@gmail.com>
2018-12-11 15:00:06 +0100

    fix(card): tests

--- projects/components/src/lib/card/card.component.spec.ts
+++ projects/components/src/lib/card/card.component.spec.ts
@@ -32,6 +32,47 @@ describe('CardComponent', () => {
   });
 
   describe('transclude', () => {
+    describe('position="card-header"', () => {
+      @Component({
+        template: '<ag-card><div position="card-header">Title</div></ag-card>',
+      })
+      class TestHostComponent {}
+
+      beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+      it('should transclude elements inside the .ag-card__header container', () => {
+        hostFixture.detectChanges();
+        cardDebugger = getCardDebugger();
+        const defaultTransclude = cardDebugger.nativeElement.querySelector(
+          '.ag-card__header'
+        );
+        expect(defaultTransclude.innerHTML).toBe(
+          '<div position="card-header">Title</div>'
+        );
+      });
+    });
+
+    describe('position="card-banner"', () => {
+      @Component({
+        template:
+          '<ag-card><img position="card-banner" src="http://via.placeholder.com/640x360"></ag-card>',
+      })
+      class TestHostComponent {}
+
+      beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+      it('should transclude elements inside the .ag-card__banner container', () => {
+        hostFixture.detectChanges();
+        cardDebugger = getCardDebugger();
+        const defaultTransclude = cardDebugger.nativeElement.querySelector(
+          '.ag-card__banner'
+        );
+        expect(defaultTransclude.innerHTML).toEqual(
+          '<img position="card-banner" src="http://via.placeholder.com/640x360">'
+        );
+      });
+    });
+
     describe('content', () => {
       @Component({
         template: '<ag-card>Content</ag-card>',
@@ -47,6 +88,24 @@ describe('CardComponent', () => {
           '.ag-card__content'
         );
         expect(defaultTransclude.innerText).toBe('Content');
+      });
+    });
+
+    describe('position="card-footer"', () => {
+      @Component({
+        template: '<ag-card><div position="card-footer">Footer</div></ag-card>',
+      })
+      class TestHostComponent {}
+
+      beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+      it('should transclude elements inside the .ag-card__footer container', () => {
+        hostFixture.detectChanges();
+        cardDebugger = getCardDebugger();
+        const defaultTransclude = cardDebugger.nativeElement.querySelector(
+          '.ag-card__footer'
+        );
+        expect(defaultTransclude.innerText).toBe('Footer');
       });
     });
   });
