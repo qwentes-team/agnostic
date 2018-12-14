81cf8cb76a8d1765fe98df973d4d129730843e7e
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-12-14 12:23:29 +0100

    refactor(input): storybook

--- projects/components/src/lib/form-field/form-field.story.ts
+++ projects/components/src/lib/form-field/form-field.story.ts
@@ -2,26 +2,26 @@ import {moduleMetadata, storiesOf} from '@storybook/angular';
 import {action} from '@storybook/addon-actions';
 import {SECTION} from './../../../../../.storybook/config';
 import {FormFieldComponent} from './form-field.component';
-import {InputComponent} from '../input/input.component';
+import {InputComponent, InputDirective} from '../input/input.component';
 import {SelectComponent} from '../select/select.component';
 
 storiesOf(`${SECTION.FORM}|Form Field`, module)
   .addDecorator(
     moduleMetadata({
-      declarations: [InputComponent, FormFieldComponent, SelectComponent],
+      declarations: [InputComponent, InputDirective, FormFieldComponent, SelectComponent],
     })
   )
   .add('Demo', () => ({
     template: `
       <div>
         <ag-form-field label="Color" metaLabel="required">
-          <ag-input placeholder="Write your fav color" required="true"></ag-input>
+          <input agInput placeholder="Write your fav color" required="true">
         </ag-form-field>
         <ag-form-field label="Material" metaLabel="optional">
           <ag-select [options]="[{label: 'Wood', value: 'wood'}, {label: 'Metal', value: 'metal'}]"></ag-select>
         </ag-form-field>
         <ag-form-field label="Message">
-          <textarea placeholder="Write your message"></textarea>
+          <textarea agInput placeholder="Write your message"></textarea>
         </ag-form-field>
       </div>
     `,
