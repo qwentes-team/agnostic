070d547fc629cab47f582988858a260aeb57c246
Matteo Barone <barmat90@gmail.com>
2018-12-17 17:22:31 +0100

    test(infinite-scroll): #20

--- projects/components/src/lib/infinite-scroll/infinite-scroll.component.spec.ts
+++ projects/components/src/lib/infinite-scroll/infinite-scroll.component.spec.ts
@@ -1,24 +1,98 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {InfiniteScrollModule as NgxInfiniteScrollModule} from 'ngx-infinite-scroll';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {InfiniteScrollComponent} from './infinite-scroll.component';
+import {Component, DebugElement} from '@angular/core';
+import {getChildDebugElement} from '../../test.shared';
 
 describe('InfiniteScrollComponent', () => {
-  let component: InfiniteScrollComponent;
-  let fixture: ComponentFixture<InfiniteScrollComponent>;
+  let hostFixture: ComponentFixture<any>;
+  let hostComponent: any;
+  let hostElement: HTMLElement;
+  let infiniteScrollDebugger: DebugElement;
 
-  beforeEach(async(() => {
+  const setupBeforeEachTestWithHostComponent = HostComponentClass => {
     TestBed.configureTestingModule({
-      declarations: [InfiniteScrollComponent],
-    }).compileComponents();
-  }));
+      declarations: [InfiniteScrollComponent, HostComponentClass],
+      imports: [NgxInfiniteScrollModule],
+    });
+    hostFixture = TestBed.createComponent(HostComponentClass);
+    hostComponent = hostFixture.componentInstance;
+    hostElement = hostFixture.nativeElement;
+  };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(InfiniteScrollComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  const getInfiniteScrollDebugger = () => getChildDebugElement('ag-infinite-scroll').from(hostFixture);
+
+  afterEach(() => {
+    if (hostFixture && hostFixture.destroy) {
+      hostFixture.destroy();
+    }
+    hostFixture = null;
+    hostComponent = null;
+    hostElement = null;
+    infiniteScrollDebugger = null;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('[infiniteScrollDistance]', () => {
+    @Component({template: '<ag-infinite-scroll [infiniteScrollDistance]="5"></ag-infinite-scroll>'})
+    class TestHostComponent {}
+
+    beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+    it('should bind infiniteScrollDistance', () => {
+      hostFixture.detectChanges();
+      infiniteScrollDebugger = getInfiniteScrollDebugger();
+      expect(infiniteScrollDebugger.componentInstance.infiniteScrollDistance).toBe(5);
+      expect(
+        infiniteScrollDebugger.nativeElement
+          .querySelector('[infiniteScroll]')
+          .getAttribute('ng-reflect-infinite-scroll-distance')
+      ).toBe('5');
+    });
+  });
+
+  describe('[infiniteScrollThrottle]', () => {
+    @Component({template: '<ag-infinite-scroll [infiniteScrollThrottle]="100"></ag-infinite-scroll>'})
+    class TestHostComponent {}
+
+    beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+    it('should bind infiniteScrollThrottle', () => {
+      hostFixture.detectChanges();
+      infiniteScrollDebugger = getInfiniteScrollDebugger();
+      expect(infiniteScrollDebugger.componentInstance.infiniteScrollThrottle).toBe(100);
+      expect(
+        infiniteScrollDebugger.nativeElement
+          .querySelector('[infiniteScroll]')
+          .getAttribute('ng-reflect-infinite-scroll-throttle')
+      ).toBe('100');
+    });
+  });
+
+  describe('(scrolled)', () => {
+    @Component({
+      template: `
+        <div>
+          <div style="height: 300px"></div>
+          <ag-infinite-scroll (scrolled)="fakeLog()"></ag-infinite-scroll>
+        </div>
+      `,
+    })
+    class TestHostComponent {
+      fakeLog() {
+        console.log('fake log');
+      }
+    }
+
+    beforeEach(() => setupBeforeEachTestWithHostComponent(TestHostComponent));
+
+    it('should output void', () => {
+      hostFixture.detectChanges();
+      infiniteScrollDebugger = getInfiniteScrollDebugger();
+      spyOn(infiniteScrollDebugger.componentInstance.scrolled, 'emit');
+      infiniteScrollDebugger.componentInstance.scrolled.emit();
+      hostFixture.detectChanges();
+      expect(infiniteScrollDebugger.componentInstance.scrolled.emit).toHaveBeenCalled();
+    });
   });
 });
