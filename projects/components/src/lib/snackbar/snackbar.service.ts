95ba36e49b748c16d3d48f1f19cba7763fbac8f1
Roberta Gardiman <roberta.gardiman@gmail.com>
2018-12-17 17:23:29 +0100

    fix(snackbar): direction automatically taken from position

--- projects/components/src/lib/snackbar/snackbar.service.ts
+++ projects/components/src/lib/snackbar/snackbar.service.ts
@@ -1,9 +1,9 @@
-import {Inject, Injectable, Injector, Optional} from '@angular/core';
+import {Inject, Injectable, Injector} from '@angular/core';
 import {ComponentPortal, PortalInjector} from '@angular/cdk/portal';
 import {Overlay, PositionStrategy} from '@angular/cdk/overlay';
 import {SnackbarComponent} from './snackbar.component';
 import {SnackbarModule} from './snackbar.module';
-import {SnackbarConfig, SNACKBAR_CONFIG_TOKEN, SnackbarPosition, SNACKBAR_DIRECTIONS} from './snackbar-config';
+import {SnackbarConfig, SNACKBAR_CONFIG_TOKEN, SNACKBAR_DIRECTIONS, SnackbarPosition} from './snackbar-config';
 import {SnackbarRef} from './snackbar-ref';
 
 @Injectable({
@@ -21,13 +21,24 @@ export class SnackbarService {
 
   public showSnackbar(data: SnackbarConfig) {
     this.position = {...this.snackbarConfig.position, ...data.position};
-    const overlayRef = this.overlay.create({positionStrategy: this.positionAdapter(this.position, data.direction)});
+
+    data.direction = this.setDirection(this.position, this.snackbarConfig.direction);
+
+    const strategy: PositionStrategy = this.isPositionStrategy(this.snackbarConfig.position || data.position)
+      ? (data.position as PositionStrategy)
+      : this.positionAdapter(this.position, data.direction);
+
+    const overlayRef = this.overlay.create({positionStrategy: strategy});
     const snackbarRef = new SnackbarRef(overlayRef);
     const injector = this.getInjector(data, snackbarRef, this.injector);
     const snackbarPortal = new ComponentPortal(SnackbarComponent, null, injector);
     this.lastSnackbarRef = snackbarRef;
     overlayRef.attach(snackbarPortal);
     return snackbarRef;
+  }
+
+  private setDirection(position, defaultDirection) {
+    return position.bottom !== '' ? SNACKBAR_DIRECTIONS.FROM_BOTTOM : defaultDirection;
   }
 
   private getInjector(data: SnackbarConfig, snackbarRef: SnackbarRef, parentInjector: Injector): PortalInjector {
@@ -37,14 +48,16 @@ export class SnackbarService {
     return new PortalInjector(parentInjector, tokens);
   }
 
-  private isPositionStrategy(position) {
+  private isPositionStrategy(position): boolean {
     if (typeof (position as PositionStrategy).dispose === 'function') {
-      return position as PositionStrategy;
+      return Boolean(position as PositionStrategy);
     }
   }
 
   public positionAdapter(position: SnackbarPosition | PositionStrategy, direction: string): PositionStrategy {
-    this.isPositionStrategy(position);
+    if (typeof (position as PositionStrategy).dispose === 'function') {
+      return position as PositionStrategy;
+    }
     const global = this.overlay.position().global();
     if ((position as SnackbarPosition).top) {
       global.top(this.getPositionRef(direction));
@@ -65,7 +78,7 @@ export class SnackbarService {
     return typeof value === 'number' ? value + 'px' : value;
   }
 
-  private getPositionRef(direction = SNACKBAR_DIRECTIONS.FROM_TOP): string {
+  private getPositionRef(direction): string {
     const lastSnackbarRefIsVisible = this.lastSnackbarRef && this.lastSnackbarRef.isVisible();
 
     if (!lastSnackbarRefIsVisible) {
