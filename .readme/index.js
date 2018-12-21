cfdb87677383cd34da2b2e32c4a2eeebaaa14db5
Matteo Barone <barmat90@gmail.com>
2018-12-21 15:53:04 +0100

    feat(readme): script (description) | #35

--- .readme/index.js
+++ .readme/index.js
@@ -13,6 +13,13 @@ const help = yargs
   .epilog(`Copyright ${new Date().getFullYear()} Qwentes`)
   .require('title').argv;
 
+const buildComponentTitle = componentFileContent => {
+  return componentFileContent
+    .split('\n')
+    .find(line => line.includes('selector: '))
+    .match(/'([^']+)'/)[1];
+};
+
 const buildComponentNameExample = title => {
   const snakeToCamelString = Util.snakeToCamel(title);
   return Util.capitalizeFirstLetter(snakeToCamelString);
@@ -33,57 +40,64 @@ const buildHtmlTemplateUsage = storyFileContent => {
     .slice(1, -1);
 };
 
+const buildDescription = moduleFileContent => {
+  // todo:gestire description null
+  return moduleFileContent
+    .match(/\/\*([\s\S]*?)\\*\//gm)[0]
+    .split('\n')
+    .slice(1, -1)
+    .join('\n');
+};
+
 const title = yargs.argv.title;
 
 const TEMPLATE_CONSTANTS = {
   TITLE: 'TITLE',
   DESCRIPTION: 'DESCRIPTION',
-  HTML_TEMPLATE_USAGE: 'HTML_TEMPLATE_USAGE',
   COMPONENT_NAME_EXAMPLE: 'COMPONENT_NAME_EXAMPLE',
-  CSS_CUSTOM_PROPERTIES: 'CSS_CUSTOM_PROPERTIES',
+  HTML_TEMPLATE_USAGE: 'HTML_TEMPLATE_USAGE',
   PROPERTIES: 'PROPERTIES',
   EVENTS: 'EVENTS',
+  CSS_CUSTOM_PROPERTIES: 'CSS_CUSTOM_PROPERTIES',
 };
 
 const valueToInsertInReadme = {
   componentTitle: '',
+  componentDescription: '',
   componentNameExample: '',
-  cssCustomProperties: '',
   htmlTemplateUsage: '',
+  cssCustomProperties: '',
 };
 
 const basePath = `${__dirname}/../projects/components/src/lib/${title}`;
 const baseTemplatePath = `${__dirname}/template.md`;
+const modulePath = `${basePath}/${title}.module.ts`;
 const componentPath = `${basePath}/${title}.component.ts`;
 const scssPath = `${basePath}/${title}.component.scss`;
 const storyPath = `${basePath}/${title}.story.ts`;
 
 const baseTemplateFileContent = fs.readFileSync(baseTemplatePath, 'utf8');
+const moduleFileContent = fs.readFileSync(modulePath, 'utf8');
 const componentFileContent = fs.readFileSync(componentPath, 'utf8');
 const scssFileContent = fs.readFileSync(scssPath, 'utf8');
 const storyFileContent = fs.readFileSync(storyPath, 'utf8');
 
-const componentFileContentArray = componentFileContent.split('\n').filter(line => line);
-
-valueToInsertInReadme.componentTitle = componentFileContentArray
-  .find(line => line.includes('selector: '))
-  .match(/'([^']+)'/)[1];
-
+valueToInsertInReadme.componentTitle = buildComponentTitle(componentFileContent);
+valueToInsertInReadme.componentDescription = buildDescription(moduleFileContent);
 valueToInsertInReadme.componentNameExample = buildComponentNameExample(title);
+valueToInsertInReadme.htmlTemplateUsage = buildHtmlTemplateUsage(storyFileContent);
 valueToInsertInReadme.cssCustomProperties = buildCssCustomProperties(
   scssFileContent,
   valueToInsertInReadme.componentTitle
 );
-valueToInsertInReadme.htmlTemplateUsage = buildHtmlTemplateUsage(storyFileContent);
 
 const readmeFileContent = baseTemplateFileContent
   .replace(TEMPLATE_CONSTANTS.TITLE, valueToInsertInReadme.componentTitle)
+  .replace(TEMPLATE_CONSTANTS.DESCRIPTION, valueToInsertInReadme.componentDescription)
   .replace(new RegExp(TEMPLATE_CONSTANTS.COMPONENT_NAME_EXAMPLE, 'g'), valueToInsertInReadme.componentNameExample)
   .replace(TEMPLATE_CONSTANTS.CSS_CUSTOM_PROPERTIES, valueToInsertInReadme.cssCustomProperties)
   .replace(TEMPLATE_CONSTANTS.HTML_TEMPLATE_USAGE, valueToInsertInReadme.htmlTemplateUsage);
 
-//fs.writeFileSync(`${basePath}/README.md`, readmeFileContent);
-
-console.log(readmeFileContent);
+fs.writeFileSync(`${basePath}/README.md`, readmeFileContent);
 
 console.log(`File created in the component ${title} folder`);
