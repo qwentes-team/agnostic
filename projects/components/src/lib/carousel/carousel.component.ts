de9561917147017a23dc18e5f91d043b0818172b
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-12-21 15:56:42 +0100

    fix(carousel): AgCarouselConfig dots prop | #19

--- projects/components/src/lib/carousel/carousel.component.ts
+++ projects/components/src/lib/carousel/carousel.component.ts
@@ -19,6 +19,7 @@ export interface AgCarouselConfig {
   draggable?: boolean;
   multipleDrag?: boolean;
   loop?: boolean;
+  dots?: boolean;
 }
 
 export const CAROUSEL_SYMBOL = '[[CarouselSymbol]]';
